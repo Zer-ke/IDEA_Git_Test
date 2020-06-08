ed4d41aa0fc098185aa3b3d0b4e0ac01d9ef0c80
zerke <1029537503@qq.com>
2020-06-08 22:28:14 +0800

    IDEA Git Test

---
+++ src/main/java/cn/zerke/test/GitTest.java
@@ -0,0 +1,12 @@
+package cn.zerke.test;
+
+/**
+ * @author Zerke
+ * @create 2020-06-08-22:19
+ */
+public class GitTest {
+
+    public void testGit(){
+        System.out.println("IDEA Git ...");
+    }
+}
---
+++ src/test/java/cn/zerke/testG/GitTestTest.java
@@ -0,0 +1,19 @@
+package cn.zerke.testG;
+
+import cn.zerke.test.GitTest;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Zerke
+ * @create 2020-06-08-22:22
+ */
+public class GitTestTest {
+
+    @Test
+    public void testGit() {
+        GitTest gitTest = new GitTest();
+        gitTest.testGit();
+    }
+}
